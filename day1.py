575d582d0c2f2af0c67eaafa46423e78fe50ab73
thevedants <130640597+thevedants@users.noreply.github.com>
2024-12-23 21:38:26 +0530

    Added day 1

--- day1.py
+++ day1.py
@@ -13,4 +13,15 @@ counter = 0
 n = len(numbers1)
 for i in range(n):
     counter += math.fabs(numbers1[i] - numbers2[i])
-print(int(counter))
+print("Answer to part 1 =", int(counter))
+
+M = {}
+for i in range(n):
+    if i != 0 and numbers2[i - 1] == numbers2[i]:
+        M[numbers2[i]] += 1
+    else:
+        M[numbers2[i]] = 1
+counter2 = 0
+for i in range(n):
+    counter2 += numbers1[i] * M.get(numbers1[i], 0)
+print("Answer to part 2 =", int(counter2))
